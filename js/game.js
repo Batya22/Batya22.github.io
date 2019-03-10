657cf2920e6a50b6ffbc1c0a2dc4615e2896e13c
Batya <makxzm@gmail.com>
2019-03-10 22:55:01 +0700

    музон, фон,

--- js/game.js
+++ js/game.js
@@ -5,23 +5,30 @@ var ctx = cvs.getContext("2d");
 var bg = new Image();
 var delor = new Image();
 
-delor.src = "../img/delor.png"
+delor.src = "../img/delor1.png"
 bg.src = "../img/bg.png"
 
-//нажаите на клавишу
+//нажатие на клавишу
 document.addEventListener("keydown", moveUp);
 document.addEventListener ("touchstart", moveUp)
+// аудио
+var music = new Audio();
+
+music.src = "../audio/NC.mp3"
 function moveUp(){
     yPos -=20;
 }
 
+
 //позиция машины
-var xPos= 0;
+var xPos= 0; 
 var yPos= 520;
 function draw() {
-    ctx.drawImage(bg, 0, 0);
+   // ctx.drawImage(bg, 0, 0);
     ctx.drawImage(delor, xPos, yPos)
+    music.play();
     requestAnimationFrame(draw)
 }
 
-delor.onload= draw;
+delor.onload= draw; 
+// создание экземпляра движка
