8ee245973786d0cc4f9e82ee509361792c229b7b
Batya <makxzm@gmail.com>
2019-03-10 23:42:09 +0700

    LOL

--- js/game.js
+++ js/game.js
@@ -9,12 +9,12 @@ delor.src = "../img/delor1.png"
 bg.src = "../img/bg.png"
 
 //нажатие на клавишу
-document.addEventListener("keydown", moveUp);
-document.addEventListener ("touchstart", moveUp)
+//document.addEventListener("keydown", moveUp);
+//document.addEventListener ("touchstart", moveUp)
 // аудио
 var music = new Audio();
 
-music.src = "../audio/NC.mp3"
+music.src = "../audio/intcut.mp3"
 function moveUp(){
     yPos -=20;
 }
@@ -22,9 +22,14 @@ function moveUp(){
 
 //позиция машины
 var xPos= 0; 
-var yPos= 520;
+var yPos= 870;
 function draw() {
    // ctx.drawImage(bg, 0, 0);
+   window.setInterval(function () {
+    if (!this.a || this.a < 0) this.a = 30000; //ширина img
+    else this.a--;
+    document.getElementById("canvas").style.backgroundPosition = this.a + "px 0px" //высота, можно в %%, если родительский элемент тоже в %
+    }, 170); //скорость
     ctx.drawImage(delor, xPos, yPos)
     music.play();
     requestAnimationFrame(draw)
